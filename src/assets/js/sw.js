f35e3a1a94117664cfa9d147ece1b5a29cf3fac3
Kenzo Umezawa <kenzoumezawa@gmail.com>
2025-10-25 14:41:14 -0300

    feat: implement fetch event handling in service worker

--- src/assets/js/sw.js
+++ src/assets/js/sw.js
@@ -7,3 +7,9 @@ self.addEventListener('activate', event => {
     console.log('Service Worker activated');
     return self.clients.claim();
 });
+
+self.addEventListener('fetch', event => {
+    console.log('Fetching:', event.request.url);
+    console.log('accept:', event.request.headers.get('accept'));
+    event.respondWith(fetch(event.request));
+});
